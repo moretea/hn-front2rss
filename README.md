960e2912e22ae6b043403b00a572bdf00866a321
Maarten Hoogendoorn <maarten@moretea.nl>
2017-01-09 14:10:27 +0100

    Docs & license

---
+++ README.md
@@ -0,0 +1,16 @@
+#hn-front2rss
+
+This goal of this project is to reduce the time I'm spending on Hacker News.
+I really love the quality of the submissions and the interesting discussions about them.
+
+However, reading HN is just like gambling. Sometimes you win, but most of the times you loose 
+(read: most of the time there are no new interesting articles).
+
+## How it works
+This [sinatra](www.sinatrarb.com) server publishes an Atom RSS feed on http://localhost:8084/hn.rss
+that contains the stories from the first two pages of the "Front" list.
+E.g. on Jan 8th 2017, it will generate RSS items for 
+https://news.ycombinator.com/front?day=2017-01-07&p=1 and
+https://news.ycombinator.com/front?day=2017-01-07&p=2.
+
+I recommend to use Tiny Tiny RSS to queue incoming stories.
--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ bash, ruby, bundlerEnv, writeScriptBin }:
+{ stdenv, bash, ruby, bundlerEnv, writeScript }:
 let
   env = bundlerEnv {
     name = "hn-rss-ruby-env";
@@ -7,8 +7,25 @@ let
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
   };
+  script = writeScript "hn-front2rss" ''
+    #!/${bash}/bin/bash
+    ${env}/bin/bundle exec ${ruby}/bin/ruby ${./server.rb} $@
+  '';
 in
-writeScriptBin "hn-front2rss" ''
-  #!/${bash}/bin/bash
-  ${env}/bin/bundle exec ${ruby}/bin/ruby ${./server.rb} $@
-''
+stdenv.mkDerivation rec {
+  version = "0.1.0";
+  name = "hn-front2rss-${version}";
+  meta = {
+    maintainer = [ "Maarten Hoogendoorn <maarten@moretea.nl>" ];
+    description = "Server that publishes an RSS of yesterday's HN front page stories";
+    license = "GPL-3.0";
+  };
+
+  phases = "linkPhase";
+
+  linkPhase = ''
+    mkdir -p $out/bin
+    ln -s ${script} $out/bin/hn-front2rss
+  '';
+}
+
